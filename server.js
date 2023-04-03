5c5847338cf1dfa827749d1ee11283eda0c66afa
spirogetto <mushfikmaruf@gmail.com>
2023-04-03 20:55:09 +0600

    Commit now

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 require('dotenv').config()
 const todorout = require('./routes/todos')
 const mongoose = require('mongoose');
@@ -15,6 +16,7 @@ db.once('open', ()=>console.log('subt'));
 
 const app = express()
 const port = 3000;
+app.use(cors())
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/todos',todorout);
