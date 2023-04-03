4b94f565dce6168607e32b31310bc31f8ca7c8a4
spirogetto <mushfikmaruf@gmail.com>
2023-04-03 22:04:38 +0600

    json parsing

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ db.once('open', ()=>console.log('subt'));
 
 
 const app = express()
-const port = 3000;
 app.use(cors())
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,6 +29,6 @@ app.post('/', (req, res) => {
   });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${process.env.PORT}`)
 });
 
